c70d72c531db93c0f9e473c6fd7ba0a50e3953be
XuXiaowen <2928477161@qq.com>
2017-12-08 22:51:55 +0800

    连接redux

--- app/containers/index.js
+++ app/containers/index.js
@@ -3,9 +3,13 @@ import {
   HashRouter as Router, //提供一个路由容器
   Route //单条路由
 } from 'react-router-dom';
-import Home from '../containers/Home/home';
-export default class App extends Component {
 
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';//绑定actionCreators组成的对象
+import * as Actions from '../store/actions/userInfos';//获取所有actionCreator组成的对象
+
+import Home from '../containers/Home/home';
+class App extends Component {
   constructor() {
     super();
     this.state = {
@@ -14,9 +18,14 @@ export default class App extends Component {
   }
 
   componentDidMount() {
+    //先去本地查找是否存储过localStorage名字叫cityName的
+    //1、第一次前没有cityName，赋予一个默认值 杭州
+    //2、将当前的城市存放到redux里//因为所有的页面都要用到
+
     this.setState({
       isLoaded: true
     })
+    //console.log(this.props.userActions);
   }
 
   render() {
@@ -29,3 +38,13 @@ export default class App extends Component {
     )
   }
 }
+export default connect(
+  state => { //mapStateToProps
+    return {}
+  },
+  dispatch => { //mapDispatchToProps
+    return {
+      userActions: bindActionCreators(Actions, dispatch)
+    }
+  }
+)(App)
